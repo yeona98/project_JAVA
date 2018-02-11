2e1669f42c6f510f6ea2357ffdc9d1fde10bd940
Han99 <senssos@daum.net>
2018-02-11 16:01:11 +0900

    180211 finished

--- Work/Javaproject/src/main/java/java04/ex04_07_합계구하기.java
+++ Work/Javaproject/src/main/java/java04/ex04_07_합계구하기.java
@@ -19,17 +19,22 @@ public class ex04_07_합계구하기 {
 		}
 		System.out.println( num1 + "부터 " + num2 + "까지의 합계는 "  + sum +"입니다");
 		
+		// Swap
+		int temp = num1;
+		num1 = num2;
+		num2 = temp;
+		
 		System.out.print("시작값을 입력하세요. ");
-		int num3 = keyboard.nextInt();
+		num2 = keyboard.nextInt();
 		System.out.print("종료값을 입력하세요. ");
-		int num4 = keyboard.nextInt();
-		int sum2 = 0;
+		num1 = keyboard.nextInt();
+		sum = 0;
 		
 		
-		for(int i = num4; i <= num3; i = i + 1) {
-			sum2 = sum2 + i;
+		for(int i = num1; i <= num2; i = i + 1) {
+			sum = sum + i;
 		}
-		System.out.println( num4 + "부터 " + num3 + "까지의 합계는 "  + sum2 +"입니다");
+		System.out.println( num1 + "부터 " + num2 + "까지의 합계는 "  + sum +"입니다");
 	}
 
 }
--- Work/Javaproject/src/main/java/java04/jv04_12_삼각형출력.java
+++ Work/Javaproject/src/main/java/java04/jv04_12_삼각형출력.java
@@ -5,12 +5,33 @@ import java.util.Scanner;
 public class jv04_12_삼각형출력 {
 	
 	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);
 		
-		for(int i = 1; i <= 10; i = i + 1) {
-			for(int j = 1; j <= 10; j = j + 1) {
-				System.out.print("*");
-			}
-			System.out.println();
+		System.out.print("Choice : ");
+		int choice = keyboard.nextInt();
+		
+		switch(choice) {
+		case 1:
+			for(int i = 1; i < 10; i = i + 1) {
+				for(int j = 1; j <= 10; j = j + 1) {
+					System.out.print("*");
+				}
+				System.out.println();
+			} break;
+		case 2:
+			for(int i = 1; i < 10; i = i + 1) {
+				for(int j = 1; j <= i; j = j + 1) {
+					System.out.print("*");
+				}
+				System.out.println();
+			} break;
+		case 3:
+			for(int i = 1; i < 10; i = i + 1) {
+				for(int j = 10; i <= j; j = j - 1) {
+					System.out.print("*");
+				}
+				System.out.println();
+			} break;
 		}
 	}
 }
